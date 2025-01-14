c5457f839f66891b04b45dfa6fbe94bca6a6d0bb
ABDELHAFID AIT EL MOKHTAR <103076950+ABDELHAFIDAIT@users.noreply.github.com>
2025-01-14 14:55:58 +0100

    Add User Class with constructor , getters & setters

---
+++ classes/user.php
@@ -0,0 +1,71 @@
+<?php
+require_once __DIR__ . './../config/db.php';
+
+class User {
+    protected int $id;
+    protected string $nom;
+    protected string $prenom;
+    protected string $telephone;
+    protected string $email;
+    protected string $password;
+    protected string $role;
+    protected string $photo;
+    protected bool $status;
+    protected $database;
+
+    public function __construct() {
+        $this->database = Database::getInstance()->getConnection();
+    }
+
+    // GETTERS
+    public function getId(): int {
+        return $this->id;
+    }
+    public function getNom(): string {
+        return $this->nom;
+    }
+    public function getPrenom(): string {
+        return $this->prenom;
+    }
+    public function getTelephone(): string {
+        return $this->telephone;
+    }
+    public function getEmail(): string {
+        return $this->email;
+    }
+    public function getPassword(): string {
+        return $this->password;
+    }
+    public function getRole(): string {
+        return $this->role;
+    }
+    public function getPhoto(): string {
+        return $this->photo;
+    }
+    public function getStatus(): bool {
+        return $this->status;
+    }
+
+    // SETTERS
+    public function setNom(string $nom): void {
+        $this->nom = $nom;
+    }
+    public function setPrenom(string $prenom): void {
+        $this->prenom = $prenom;
+    }
+    public function setTelephone(string $telephone): void {
+        $this->telephone = $telephone;
+    }
+    public function setEmail(string $email): void {
+        $this->email = $email;
+    }
+    public function setPassword(string $password): void {
+        $this->password = password_hash($password, PASSWORD_DEFAULT);
+    }
+    public function setRole(string $role): void {
+        $this->role = $role;
+    }
+    public function setStatus(string $status): void {
+        $this->status = $status;
+    }
+}
