e434d2d964fedebad03073b30482658370101485
ABDELHAFID AIT EL MOKHTAR <103076950+ABDELHAFIDAIT@users.noreply.github.com>
2025-01-14 15:15:01 +0100

    Add Tag  Class

---
+++ classes/tag.php
@@ -0,0 +1,29 @@
+<?php
+
+    require_once __DIR__ . './../config/db.php';
+    class Tag {
+        private $id;
+        private $nom;
+
+        private $database;
+
+        public function __construct() {
+            $this->database = Database::getInstance()->getConnection();
+        }    
+
+        // Getters
+        public function getId() {
+            return $this->id;
+        }
+        public function getNom() {
+            return $this->nom;
+        }
+
+        // Setters
+        public function setNom($nom) {
+            $this->nom = $nom;
+        }
+
+
+        
+    }
