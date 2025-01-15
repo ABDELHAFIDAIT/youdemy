6eaba21e9e6ee74104c6e637d5c819eb8b506485
ABDELHAFID AIT EL MOKHTAR <103076950+ABDELHAFIDAIT@users.noreply.github.com>
2025-01-15 22:05:29 +0100

    Fixer la gestion de role et l'acces aux pages du Visiteur

--- views/guest/courses.php
+++ views/guest/courses.php
@@ -1,3 +1,22 @@
+<?php
+
+    session_start();
+
+    if (isset($_SESSION["role"])){
+        if($_SESSION['role'] === 'Admin'){
+            header("Location: ../admin/dashboard.php");
+        }else if($_SESSION['role'] === 'Enseignant'){
+            header("Location: ../teacher/dashboard.php");
+        }else{
+            header("Location: ../student/courses.php");
+        }
+        exit;
+    }
+
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
