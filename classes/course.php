a28938b7c6b9da9c6594447191d89c207a38a2b8
ABDELHAFID AIT EL MOKHTAR <103076950+ABDELHAFIDAIT@users.noreply.github.com>
2025-01-16 11:48:57 +0100

    Add allCourses Method

--- classes/course.php
+++ classes/course.php
@@ -11,9 +11,18 @@
         private string $video;
         private string $status;
         private string $date;
+        private string $niveau;
         private $database;
 
-        public function __construct() {
+        public function __construct($titre,$description,$couverture,$contenu,$video,$statut,$niveau) {
+            $this->titre = $titre;
+            $this->description = $description;
+            $this->couverture = $couverture;
+            $this->contenu = $contenu;
+            $this->video = $video;
+            $this->status = $statut;
+            $this->date = $statut;
+            $this->niveau = $niveau;
             $this->database = Database::getInstance()->getConnection();
         }
 
@@ -66,5 +75,41 @@
             $this->date = $date;
         }
 
+
+        // GET ALL COURSES WITH CATEGORY NAME AND TEACHER FULL NAME
+        public function allCourses(){
+            try{
+                $query = "SELECT Co.id_course,
+                                Co.titre,
+                                Co.description,
+                                Co.couverture,
+                                Co.contenu,
+                                Co.video,
+                                Co.niveau,
+                                Co.date_publication,
+                                Co.statut_cours,
+                                Ca.nom_categorie AS categorie,
+                                CONCAT(U.prenom, ' ', U.nom) AS enseignant
+                            FROM 
+                                courses Co
+                            JOIN 
+                                categories Ca ON Co.id_categorie = Ca.id_categorie
+                            JOIN 
+                                users U ON Co.id_teacher = U.id_user
+                            ORDER BY
+                                Co.date_publication DESC, Co.id_course ASC";
+
+                $stmt = $this->database->prepare($query);
+                $stmt->execute();
+                if($stmt->rowCount() > 0){
+                    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+                    return $result;
+                }else{
+                    return false;
+                }
+            }catch(PDOException $e){
+                throw new Exception("Erreur lors de la Récupération des Cours : ". $e->getMessage());
+            }
+        }
         
     }
