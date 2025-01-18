8ef8956e2dcf5649c2648959819baf8540db2f2f
ABDELHAFID AIT EL MOKHTAR <103076950+ABDELHAFIDAIT@users.noreply.github.com>
2025-01-18 14:52:57 +0100

    Add subscribedCourses Method

--- classes/student.php
+++ classes/student.php
@@ -26,7 +26,36 @@
                 throw new Exception('Erreur lors de l\'Inscription à ce cours : ' .$e->getMessage());
             }
         }
-
+        
+        
+        // GET SUBSCRIBED COURSES
+        public function subscribedCourses($student) {
+            try {
+                $sql = 'SELECT 
+                            Co.id_course,
+                            Co.titre,
+                            Co.description,
+                            Co.couverture,
+                            Co.niveau,
+                            Ca.nom_categorie,
+                            U.prenom AS teacher_prenom,
+                            U.nom AS teacher_nom
+                        FROM enrollments E
+                        INNER JOIN courses Co ON E.id_course = Co.id_course
+                        INNER JOIN categories Ca ON Co.id_categorie = Ca.id_categorie
+                        INNER JOIN users U ON Co.id_teacher = U.id_user
+                        WHERE E.id_student = :student';
+                
+                $query = $this->database->prepare($sql);
+                $query->bindParam(':student', $student, PDO::PARAM_INT);
+                $query->execute();
+                
+                $courses = $query->fetchAll(PDO::FETCH_ASSOC);
+                return $courses;
+            } catch (PDOException $e) {
+                throw new Exception('Erreur lors de la récupération des cours inscrits : ' . $e->getMessage());
+            }
+        }
     }
 
 ?>
