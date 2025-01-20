a127370f5a475f3b398681696e00f04e74a14bd2
ABDELHAFID AIT EL MOKHTAR <103076950+ABDELHAFIDAIT@users.noreply.github.com>
2025-01-20 19:47:07 +0100

    Add Navbar to all admin pages

--- classes/admin.php
+++ classes/admin.php
@@ -5,9 +5,10 @@
 
     class Admin extends User implements DisplayCourse{
 
-        // public function __construct(){
-        //     parent::__construct();
-        // }
+        public function __construct($id,$nom,$prenom,$telephone,$email,$password,$role,$status,$photo){
+            parent::__construct($nom,$prenom,$telephone,$email,$password,$role,$status,$photo);
+            $this->id = $id;
+        }
 
         public function displayCourses($param){
             
---
+++ views/admin/teachers.php
@@ -0,0 +1,125 @@
+<?php
+
+    session_start();
+
+    require_once "../../classes/teacher.php";
+    require_once "../../classes/admin.php";
+    require_once "../../classes/category.php";
+    require_once "../../classes/course.php";
+
+
+
+    $administrator = new Admin((int)$_SESSION['id_user'],$_SESSION['nom'],$_SESSION['prenom'],$_SESSION['phone'],$_SESSION['email'],'',$_SESSION['role'],$_SESSION['status'],$_SESSION['photo']);
+
+
+    if ($_SESSION['role'] !== 'Admin') {
+        if ($_SESSION['role'] === 'Enseignant') {
+            header("Location: ../teacher/dashboard.php");
+        } else if ($_SESSION['role'] === 'Etudiant') {
+            header("Location: ../student/");
+        } else {
+            header("Location: ../guest");
+        }
+        exit;
+    }
+
+    if($_SERVER['REQUEST_METHOD'] === 'POST') {
+        if(isset($_POST['disconnect'])) {
+            session_unset();
+            session_destroy();
+            header("Location: ../guest");
+            exit();
+        }
+    }
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Youdemy</title>
+    <link rel="icon" href="../../assets/img/logo.png">
+    <link rel="stylesheet" href="../../assets/css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body>
+    <header class="px-5 py-3 bg-white shadow-md fixed w-full">
+        <nav class="flex items-center justify-between gap-5">
+            <div class="flex items-center gap-6">
+                <div id="burger-menu">
+                    <i class="fa-solid fa-bars text-2xl cursor-pointer"></i>
+                </div>
+                <div class="flex items-center gap-1">
+                    <img class="w-10" src="../../assets/img/logo.png" alt="Logo de Youdemy Plateforme">
+                    <h1 class="text-xl font-semibold">You<span class="text-blue-800">Demy</span></h1>
+                </div>
+            </div>
+            <div class="flex items-center gap-6">
+                <a href="#"><img class="w-14 rounded-full border-4 border-blue-500" src="../../uploads/<?php echo $administrator->getPhoto() ?>" alt=""></a>
+            </div>
+        </nav>
+
+        <section id="links" style="height: calc(100vh - 80px);" class="z-10 absolute top-[80px] left-[-500px] transition-all ease-in duration-500 w-64 bg-white shadow-md py-8 flex flex-col justify-between overflow-auto">
+            <div class="flex flex-col items-center justify-center gap-2">
+                <img class="w-1/3 rounded-full border-4 border-white" src="../../uploads/<?php echo $administrator->getPhoto() ?>" alt="">
+                <h1 class="font-semibold mt-2"><?php echo $administrator->getNom().' '.$administrator->getPrenom() ?></h1>
+                <p class="text-xs">Espace Admin</p>
+            </div>
+            <div class="flex flex-col items-center justify-center gap-2 py-10">
+                <a href="dashboard.php" class="flex items-center gap-3 duration-300 hover:bg-gray-200 w-full pl-5 hover:border-r-4 hover:border-gray-400 py-3">
+                    <i class="fa-solid fa-chart-simple"></i>
+                    <p>Statistiques</p>
+                </a>
+                <a href="courses.php" class="flex items-center gap-3 duration-300 hover:bg-gray-200 w-full pl-5 hover:border-r-4 hover:border-gray-400 py-3">
+                    <i class="fa-solid fa-book-open "></i>
+                    <p>Cours</p>
+                </a>
+                <a href="categories.php" class="flex items-center gap-3 duration-300 hover:bg-gray-200 w-full pl-5 hover:border-r-4 hover:border-gray-400 py-3">
+                    <i class="fa-solid fa-layer-group"></i>
+                    <p>Catégories</p>
+                </a>
+                <a href="tags.php" class="flex items-center gap-3 duration-300 hover:bg-gray-200 w-full pl-5 hover:border-r-4 hover:border-gray-400 py-3">
+                    <i class="fa-solid fa-tags"></i>
+                    <p>Tags</p>
+                </a>
+                <a href="students.php" class="flex items-center gap-3 duration-300 hover:bg-gray-200 w-full pl-5 hover:border-r-4 hover:border-gray-400 py-3">
+                    <i class="fa-solid fa-graduation-cap"></i>
+                    <p>Etudiants</p>
+                </a>
+                <a href="#" class="flex items-center gap-3 duration-300 hover:bg-gray-200 w-full pl-5 hover:border-r-4 hover:border-gray-400 py-3">
+                    <i class="fa-solid fa-chalkboard-user"></i>
+                    <p>Enseignants</p>
+                </a>
+            </div>
+            <form method="POST" action="">
+                <button name="disconnect" class="flex items-center gap-3 duration-300 hover:bg-gray-200 w-full pl-5 hover:border-r-4 hover:border-gray-400 py-3">
+                    <i class="fa-solid fa-arrow-right-from-bracket"></i>
+                    Déconnexion
+                </button>
+            </form>
+        </section>
+    </header>
+
+    <main class="bg-gray-100 pt-24 pb-12 px-5">
+        <!-- Statistiques -->
+        
+    </main>
+
+
+    <script src="../../assets/js/main.js"></script>
+    <script>
+            let list = document.querySelector('#links');
+            const menu = document.querySelector('#burger-menu');
+
+            menu.addEventListener('click',function(){
+                list.classList.toggle('left-0');
+                list.classList.toggle('left-[-500px]');
+            });
+    </script>
+</body>
+</html>
